c2a2590a8cb4cacdaeecfede7eaeb27ace4b914f
Shai <shay4447@gmail.com>
2022-08-14 14:47:22 +0300

    .

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+import './assets/styles/styles.scss';
+
+function App() {
+  return (
+    <div className="main-app">
+            <header className='app-header'>
+                <section className='container'>
+                    <h1>misterCoin</h1>
+                </section>
+            </header>
+            <main className='container'>
+                
+            </main>
+            <footer>
+                <section className='container'>
+                  copyrights 2022 &copy;
+                </section>
+            </footer>
+        </div>
+  );
+}
+
+export default App;
